5ec7e2d0bf55108fa822ae160f3b7f10e6166e3e
Augustus Francis <AFrancis@esri.com>
2022-03-28 15:34:35 +0400

    __init__.py: Fix import

--- h5py/__init__.py
+++ h5py/__init__.py
@@ -18,7 +18,7 @@ if version_info >= (3, 8, 0) and platform == 'win32':
     import os
     if 'PATH' in os.environ:
         for p in os.environ['PATH'].split(';'):
-            if p and os.path.exists(p):
+            if p and p != '.' and p != '..' and os.path.exists(p):
                 os.add_dll_directory(p)
 
 
